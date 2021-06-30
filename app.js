b0ad1a7c59a90c670f370e3278eaf6582986bb46
dimaalabsi8 <>
2021-06-30 19:43:18 +0000

    adding if

---
+++ app.js
@@ -0,0 +1,16 @@
+var name = prompt("what's your name?")
+alert("welcome to my website " +  name)
+
+
+
+var favourite = prompt ("what about your favourite movie?")
+
+
+
+
+if(favourite == "me before you"){document.write("<p>"+ "great, let's enjoy with our favourite movie " + "</p>)"}
+
+
+
+
+else { alert("sorry this website is'not about your favourite one")}
