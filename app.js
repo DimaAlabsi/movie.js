31e35eedc53a5c2c91a03edca4f38fb30c6b7db8
DimaAlabsi <86602837+DimaAlabsi@users.noreply.github.com>
2021-07-01 14:18:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ alert("welcome to my website " +  name)
 
 
 
+
 var favourite = prompt ("what about your favourite movie?")
 
+console.log( favourite)
+
+ if(favourite == "me before you"){document.write( "<p>"+ 
+"great, let's enjoy with our favourite movie " + "</p>")}
 
 
 
-if(favourite == "me before you"){document.write("<p>"+ "great, let's enjoy with our favourite movie " + "</p>)"}
 
-
-
-
-else { alert("sorry this website is'not about your favourite one")}
+else { alert("sorry this website is not about your favourite one")}
